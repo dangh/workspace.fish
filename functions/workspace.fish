4b10823f27f8eb702fdfe9194905665625240541
Dang <dang@travelstop.com>
2020-09-08 04:01:20 +0800

    support removing workspace

--- functions/workspace.fish
+++ functions/workspace.fish
@@ -1,22 +1,24 @@
-function workspace --argument-names branch
-  switch $branch
-    case --setup
-      _workspace_setup
-    case ""
+function workspace --argument-names command
+  switch $command
+    case init
+      _workspace_init
+    case rm remove
+      _workspace_remove "$argv[2..-1]"
+    case "" ls list
       _workspace_list
     case \*
-      _workspace_switch $branch
+      _workspace_switch $command
   end
 end
 
-function _workspace_setup
+function _workspace_init
   if string match --quiet --regex '/\.ws(/|$)' (pwd -P)
     echo [workspace] workspace already initialized!
     return
   end
 
-  set --local workspace_root (git rev-parse --show-toplevel)
-  set --local workspace (_workspace_name (git branch --show-current))
+  set --local workspace_root (command git rev-parse --show-toplevel)
+  set --local workspace (_workspace_name (command git branch --show-current))
   command mv "$workspace_root" "{$workspace_root}-tmp"
   command mkdir -p "$workspace_root/.ws"
   command mv "{$workspace_root}-tmp" "$workspace_root/.ws/$workspace"
@@ -35,9 +37,9 @@ function _workspace_list
   command git -C "$workspace_root/.ws/$git_working_dir" worktree list
 end
 
-function _workspace_switch --argument-name branch
+function _workspace_switch --argument-names branch
   set --local workspace_root (_workspace_root)
-  set --local workspace (_workspace_name $branch)
+  set --local workspace (_workspace_name "$branch")
   set --local git_working_dir (cat "$workspace_root/.ws/git_working_dir")
   if test ! -d "$workspace_root/.ws/$workspace"
     echo [workspace] creating workspace $workspace
@@ -49,6 +51,21 @@ function _workspace_switch --argument-name branch
     end
   end
   cd "$workspace_root/$workspace"
+end
+
+function _workspace_remove --argument-names branch
+  set --local workspace_root (_workspace_root)
+  set --local workspace (_workspace_name "$branch")
+  set --local git_working_dir (cat "$workspace_root/.ws/git_working_dir")
+  set --local current_workspace (_workspace_name (command git branch --show-current))
+  if test -d "$workspace_root/.ws/$workspace"
+    command git -C "$workspace_root/.ws/$git_working_dir" worktree remove "$workspace"
+    command rm "$workspace_root/$workspace"
+  end
+  if test "$workspace" = "$current_workspace"
+    cd "$workspace_root"
+  end
+  _workspace_list
 end
 
 function _workspace_root
