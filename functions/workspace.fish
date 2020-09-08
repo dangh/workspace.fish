9efbde25211d9a30d537789679f62c0264c3ec4e
Dang <dang@travelstop.com>
2020-09-08 18:23:11 +0800

    add/checkout command

--- functions/workspace.fish
+++ functions/workspace.fish
@@ -2,19 +2,23 @@ function workspace --argument-names command
   switch $command
     case init
       _workspace_init
+    case add
+      _workspace_add $argv[2..-1]
     case rm remove
-      _workspace_remove "$argv[2..-1]"
-    case "" ls list
-      _workspace_list
+      _workspace_remove $argv[2..-1]
+    case ls list ""
+      _workspace_list $arvg[2..-1]
+    case co checkout
+      _workspace_checkout $argv[2..-1]
     case \*
-      _workspace_switch $command
+      _workspace_checkout $argv
   end
 end
 
 function _workspace_init
   if string match --quiet --regex '/\.ws(/|$)' (pwd -P)
-    echo [workspace] workspace already initialized!
-    return
+    _workspace_log workspace already initialized!
+    return 1
   end
 
   set --local workspace_root (command git rev-parse --show-toplevel)
@@ -23,7 +27,7 @@ function _workspace_init
   command mkdir -p "$workspace_root/.ws"
   command mv "{$workspace_root}-tmp" "$workspace_root/.ws/$workspace"
   echo "$workspace" > "$workspace_root/.ws/git_working_dir"
-  command ln -s "$workspace_root/.ws/$workspace" "$workspace_root/$workspace"
+  command ln -sf "$workspace_root/.ws/$workspace" "$workspace_root/$workspace"
   if set --query ws_setup_script
     cd "$workspace_root/.ws/$workspace"
     echo "$ws_setup_script" | source
@@ -37,23 +41,38 @@ function _workspace_list
   command git -C "$workspace_root/.ws/$git_working_dir" worktree list
 end
 
-function _workspace_switch --argument-names branch
+function _workspace_add --argument-names branch
   set --local workspace_root (_workspace_root)
   set --local workspace (_workspace_name "$branch")
   set --local git_working_dir (cat "$workspace_root/.ws/git_working_dir")
   if test ! -d "$workspace_root/.ws/$workspace"
-    echo [workspace] creating workspace $workspace
+    _workspace_log creating workspace `$workspace`
     command git -C "$workspace_root/.ws/$git_working_dir" worktree add -B "$branch" --guess-remote --quiet "$workspace_root/.ws/$workspace" "$branch"
-    command ln -s "$workspace_root/.ws/$workspace" "$workspace_root/$workspace"
+    command ln -sf "$workspace_root/.ws/$workspace" "$workspace_root/$workspace"
     if set --query ws_setup_script
       cd "$workspace_root/.ws/$workspace"
       echo "$ws_setup_script" | source
     end
+  else
+    _workspace_log workspace `$workspace` is already exists! checkout `$workspace`
+  end
+  _workspace_checkout $branch
+end
+
+function _workspace_checkout --argument-names branch
+  _workspace_log checkout `$branch`
+  set --local workspace_root (_workspace_root)
+  set --local workspace (_workspace_name "$branch")
+  if test ! -d "$workspace_root/.ws/$workspace"
+    _workspace_log workspace `$workspace` does not exist!
+    _workspace_log checkout `$workspace` as (set_color magenta)workspace add $workspace(set_color normal)
+    return 1
   end
   cd "$workspace_root/$workspace"
 end
 
 function _workspace_remove --argument-names branch
+  _workspace_log remove `$branch`
   set --local workspace_root (_workspace_root)
   set --local workspace (_workspace_name "$branch")
   set --local git_working_dir (cat "$workspace_root/.ws/git_working_dir")
@@ -71,7 +90,7 @@ end
 function _workspace_root
   set --local workspace_root (string replace --regex "/.ws/.*" "" -- (pwd -P))
   if test ! -d "$workspace_root/.ws"
-    echo [workspace] not a git workspace!
+    _workspace_log not a git workspace!
     return 1
   end
   echo $workspace_root
@@ -79,4 +98,8 @@ end
 
 function _workspace_name --argument-names branch
   echo $branch
+end
+
+function _workspace_log
+  echo '('(set_color green)workspace(set_color normal)')' $argv
 end
