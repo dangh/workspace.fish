2dafb2c871b7921962b769939adee776bd811275
Dang <dang@travelstop.com>
2020-10-07 20:28:44 +0800

    fix completion for remove

--- completions/workspace.fish
+++ completions/workspace.fish
@@ -1,5 +1,11 @@
 function __workspace_complete_list
-  for i in (git worktree list)
+  set --local git_working_dir
+  set --local git_args
+  if test -e .ws/git_working_dir
+    set git_working_dir .ws/(command cat .ws/git_working_dir)
+    set git_args "-C" $git_working_dir
+  end
+  for i in (command git $git_args worktree list)
       string replace -r '^.*/.ws/(.+?)\s+\w{9}\s\[.+\]$' '$1' $i
   end
 end
@@ -8,7 +14,7 @@ function __workspace_complete_add
   git branch --remote --format '%(refname:lstrip=3)'
 end
 
-set --local subcommands init add rm ls list co checkout
+set --local subcommands init add rm remove ls list co checkout
 
 complete --keep-order --no-files --command workspace --condition "not __fish_seen_subcommand_from $subcommands; and not __fish_seen_subcommand_from (__workspace_complete_list)" --arguments remove --description "remove current worktree"
 complete --keep-order --no-files --command workspace --condition "not __fish_seen_subcommand_from $subcommands; and not __fish_seen_subcommand_from (__workspace_complete_list)" --arguments checkout --description "checkout worktree"
@@ -16,7 +22,7 @@ complete --keep-order --no-files --command workspace --condition "not __fish_see
 complete --keep-order --no-files --command workspace --condition "not __fish_seen_subcommand_from $subcommands; and not __fish_seen_subcommand_from (__workspace_complete_list)" --arguments list --description "list all worktrees"
 complete --keep-order --no-files --command workspace --condition "not __fish_seen_subcommand_from $subcommands; and not __fish_seen_subcommand_from (__workspace_complete_list)" --arguments init --description "init workspace"
 
-complete --keep-order --no-files --command workspace --condition "__fish_seen_subcommand_from checkout co; and not __fish_seen_subcommand_from (__workspace_complete_list)" --arguments "(__workspace_complete_list)"
+complete --keep-order --no-files --command workspace --condition "__fish_seen_subcommand_from checkout co rm remove; and not __fish_seen_subcommand_from (__workspace_complete_list)" --arguments "(__workspace_complete_list)"
 complete --keep-order --no-files --command workspace --condition "__fish_seen_subcommand_from add; and not __fish_seen_subcommand_from (__workspace_complete_add)" --arguments "(__workspace_complete_add)"
 
 complete --keep-order --no-files --command workspace --condition "not __fish_seen_subcommand_from $subcommands; and not __fish_seen_subcommand_from (__workspace_complete_list)" --arguments "(__workspace_complete_list)" --description "checkout"
